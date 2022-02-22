368d9a5127b39f154d81db3e7f31179157315baa
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-02-22 09:59:43 +0700

    Add router

--- main.go
+++ main.go
@@ -1,11 +1,42 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/julienschmidt/httprouter"
+	"github.com/sumitroajiprabowo/go-restfull-api/app"
+	"github.com/sumitroajiprabowo/go-restfull-api/controller"
+	"github.com/sumitroajiprabowo/go-restfull-api/exception"
+	"github.com/sumitroajiprabowo/go-restfull-api/helper"
+	"github.com/sumitroajiprabowo/go-restfull-api/repository"
+	"github.com/sumitroajiprabowo/go-restfull-api/service"
+)
+
 func main() {
 
-	// categoryRepository := repository.CategoryRepository()
-	// categoryService := service.NewCategoryService(categoryRepository, db, validate)
-	// categoryController := controller.NewCategoryController(categoryService)
+	db := app.NewDB()
+	validate := validator.New()
+	categoryRepository := repository.NewCategoryRepository()
+	categoryService := service.NewCategoryService(categoryRepository, db, validate)
+	categoryController := controller.NewCategoryController(categoryService)
 
-	// router := httprouter.New()
+	router := httprouter.New()
+	router.GET("/api/categories", categoryController.FindAll)
+	router.GET("/api/categories/:categoryId", categoryController.FindById)
+	router.POST("/api/categories", categoryController.Create)
+	router.PUT("/api/categories/:categoryId", categoryController.Update)
+	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+
+	router.PanicHandler = exception.PanicHandler
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
 
 }
