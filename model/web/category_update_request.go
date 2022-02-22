4a417e13312ad9e44ed810f13db07b9dea19fdc4
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-02-22 10:02:31 +0700

    Update tag

--- model/web/category_update_request.go
+++ model/web/category_update_request.go
@@ -2,5 +2,5 @@ package web
 
 type CategoryUpdateRequest struct {
 	Id   int64  `validate:"required"`
-	Name string `validate:"required, max=200, min=1"`
+	Name string `validate:"required,min=1,max=100" json:"name"`
 }
